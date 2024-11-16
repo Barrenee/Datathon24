2b739c6a6325cd93f4740db8d3d71d506365029c
Pololo04 <133002546+Pololo04@users.noreply.github.com>
2024-11-16 17:16:59 +0100

    push tontorron

--- Group.py
+++ Group.py
@@ -11,8 +11,20 @@ class Group:
         self.interest_in_challenges: set[str] = participant.interest_in_challenges
         self.preferred_languages: set[str] = participant.preferred_languages
 
+        self.programming_level: dict = participant.programming_level
+
     def add_group(self, group: Group):
         '''Merges both groups and actualizes the group's attributes'''
-        self.people_in_group.extend(group.people_in_group)
-        self.roles_fullfilled.extend(group.roles_fullfilled)          
+        self.people_in_group.extend(group.people_in_group) # Add people to the group
+        self.roles_fullfilled.extend(group.roles_fullfilled) # Add roles to the group
+        self.preferred_languages = self.preferred_languages.intersection(group.preferred_languages) # Intersect languages
+        # TODO: Objectives
+        # TODO: Interest in challenges
 
+    
+    def update_programming_level(self, group: Group, way_of_update: function = lambda x, y: (x + y)/2):
+        for key, value in self.programming_level.items():
+            self.programming_level[key] = way_of_update(group.programming_level[key], value) 
+
+    def update_objectives(self, group: Group):
+        self.objective = self.objective.intersection(group.objective)
