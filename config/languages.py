de4166f5bb4930883be72983a438b05624fe8457
barrenee <barrenecheapereapablo@gmail.com>
2024-11-16 17:12:01 +0100

    MOD: utils en carpeta + ADD languages

---
+++ config/languages.py
@@ -0,0 +1,8 @@
+LANGUAGES = [
+    "Arabic", "Bengali", "Bulgarian", "Catalan", "Chinese", "Czech", "Danish", "Dutch",
+    "English", "Finnish", "French", "German", "Greek", "Gujarati", "Hebrew", "Hindi",
+    "Hungarian", "Indonesian", "Italian", "Japanese", "Kannada", "Korean", "Malay",
+    "Marathi", "Norwegian", "Polish", "Portuguese", "Punjabi", "Romanian", "Russian",
+    "Slovak", "Spanish", "Swahili", "Swedish", "Tamil", "Telugu", "Thai", "Turkish",
+    "Ukrainian", "Urdu", "Vietnamese", "Zulu"
+]
