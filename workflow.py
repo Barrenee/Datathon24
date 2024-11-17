da7fe8dc607d4b5ce29645cec9c58905f24b1dd0
adraco007 <adriacantarero@gmail.com>
2024-11-17 02:55:04 +0100

    MOD ADD todo me vuelvo loco
    Co-authored-by: Pablo Barrenechea <Barrenee@users.noreply.github.com>

--- workflow.py
+++ workflow.py
@@ -1,5 +1,5 @@
 from config.literals import LANGUAGES
-from utils.matchesCompulsory import languages_match, objectives_match, challenges_match, find_obligatory_compatibles, init_tables
+from utils.matchesCompulsory import find_obligatory_compatibles, init_tables, get_compatible_groups
 from utils.abstraction import abstract_objective, abstract_expertise, abstract_tryhard, abstract_general, init_participant
 from Group import Group
 from participant import Participant, load_participants
@@ -15,31 +15,9 @@ g2 = init_participant(data[1])
 
 all_groups = [g1, g2]
 
-init_tables(all_groups)
+challenges_table, objectives_table, languages_table = init_tables(all_groups)
 
-"""
-table_language = languages_match(all_groups) # {language: [group1, group2, ...]}
-table_objectives = objectives_match(all_groups) # {objective: [group1, group2, ...]}
-table_challenges = challenges_match(all_groups) # {challenge: [group1, group2, ...]}
-"""
-obligatory_compatibles = find_obligatory_compatibles(all_groups, table_language, table_objectives, table_challenges)
-# enter program -> assign to group / look for friends in group
-# Texto juntado (obj intro technical future excitement fun fact) => LLM 
-# OBJECTIVE_ABSTRACT => A partir de (obj intro technical future excitement fun fact) => "Win" "Fun" "Learn" "Friends"
-# 
-
-# ABSTRACCIÓN Tryhard: if win only => extreme
-#                      if win +1 => medium
-#                      if win +2 => low
-#                      else      => none
-
-# ABSTRACCIÓN Expertise: if year=> extreme
+compatible_groups = get_compatible_groups(g1, challenges_table, objectives_table, languages_table)
 
 
-# MATCHES:
-
-# -> BASIC MATCHES:
-
-print(compatibles)
-print(table_language)
-print(table_objectives)
+print(compatible_groups)
